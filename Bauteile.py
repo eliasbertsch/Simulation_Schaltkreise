7f894017e7ecb319434924931a23bab1833d471b
snalcoo <94040887+snalcoo@users.noreply.github.com>
2022-01-31 14:51:58 +0100

    Funktion Bauteile kommentiert

--- Bauteile.py
+++ Bauteile.py
@@ -1,5 +1,5 @@
-def Lampe(LX,LY):
-    circle(LX,LY,100)
-    line(LX - 35,LY - 35, LX + 35, LY + 35)
-    line(LX - 35,LY + 35, LX + 35, LY - 35)
+def Lampe(LX,LY):                                #Definition der Funktion Lampe, welche an einer beliebigen Kombination aus X- und Y- Werten eingebaut werden kann
+    circle(LX,LY,100)                            #Kreis zeichnen mit Durchmesser 100 um den Punkt aus dem gewählten X- und Y- Wert 
+    line(LX - 35,LY - 35, LX + 35, LY + 35)      #Diagonale Linie zeichnen abhängig von dem gewählten X- und Y- Wert
+    line(LX - 35,LY + 35, LX + 35, LY - 35)      #Diagonale Linie zeichnen abhängig von dem gewählten X- und Y- Wert
     
